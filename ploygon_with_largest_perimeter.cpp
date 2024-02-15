d811dbe7a104dccfb8a6683af1f9181a34dc1d20
roushanku <roushankuiitbhilai.ac.in>
2024-02-15 08:49:31 +0530

    prefix sum

---
+++ ploygon_with_largest_perimeter.cpp
@@ -0,0 +1,46 @@
+// J.S.R -- *roush*
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+const int mod = 1000000007;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds; //find_by_order //order_of_key
+
+class Solution {
+public:
+    long long largestPerimeter(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        
+        vector <long long> pre(n , 0);
+        pre[0] = nums[0];
+        
+        for(int i = 1 ; i < n ; i++) {
+            pre[i] = pre[i-1] + nums[i];
+        }
+        
+        long long ans = -1;
+        // int end = nums[n-1];
+        for(int i = 1 ; i < n-1 ; i++) {
+            if(pre[i] > nums[i+1]) ans = max(ans , pre[i] + nums[i+1]);
+        }
+        
+        return ans;
+        
+    }
+};
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+//patience Bro , thoda sa logic soch k try ur own test case
+//hadbad m kaafi gadbad kie ho islie aaram s -> You will do best for Youself
+//First, solve the problem. Then, write the code.
+int t;
+cin>>t;
+while(t--){
+           
+}
+return 0;
+}
