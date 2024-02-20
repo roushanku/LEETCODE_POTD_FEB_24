aefb725dfd6310e7100ca6844a322b3250ca6acb
roushanku <roushankuiitbhilai.ac.in>
2024-02-20 08:48:52 +0530

    bit magic

---
+++ missing_number.cpp
@@ -0,0 +1,40 @@
+// J.S.R -- *roush*
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+const int mod = 1000000007;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds; //find_by_order //order_of_key
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+
+        //concept use : xor of num from 0...n and xor of in given array will give missing num
+
+        for(int i = 0 ; i < n ; i++) {
+            ans = ans ^ i ^ nums[i];
+        }
+
+        ans = ans ^ n;
+
+        return ans;
+    }
+};
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+//patience Bro , thoda sa logic soch k try ur own test case
+//hadbad m kaafi gadbad kie ho islie aaram s -> You will do best for Youself
+//First, solve the problem. Then, write the code.
+int t;
+cin>>t;
+while(t--){
+           
+}
+return 0;
+}
